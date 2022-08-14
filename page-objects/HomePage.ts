bb58d4eabb3cd0d1b80b85c5195eee6073b5df3a
kudokuna <htcakuma@gmail.com>
2022-08-15 02:21:34 +0300

    Refactor search spec Added a new items in HomePage obejct

--- page-objects/HomePage.ts
+++ page-objects/HomePage.ts
@@ -3,17 +3,24 @@ import { expect, Locator, Page } from "@playwright/test"
 export class HomePage {
     readonly page: Page
     readonly signInButton: Locator
+    readonly searchBox: Locator
 
     constructor(page: Page) {
         this.page = page
         this.signInButton = page.locator("#signin_button")
+        this.searchBox = page.locator("#searchTerm")
     }
 
     async visit() {
-        await this.page.goto("http://zero.webappsecurity.com/")
+        await this.page.goto("http://zero.webappsecurity.com")
     }
 
     async clickOnSignIn() {
         await this.signInButton.click()
+    }
+
+    async searchForPhrase(phrase: string) {
+        await this.searchBox.type(phrase)
+        await this.page.keyboard.press("Enter")
     }
 }
--- tests/e2e/e2e-search.spec.ts
+++ tests/e2e/e2e-search.spec.ts
@@ -1,34 +1,30 @@
 import {test, expect} from "@playwright/test"
+import {HomePage} from "../../page-objects/HomePage"
 
-test.describe.parallel("Search Result", () => {
-
-    // go to url
+test.describe.parallel.only("Search Result", () => {
+    let homePage: HomePage
+    
     test.beforeEach(async ({page}) => { 
-    await page.goto("http://zero.webappsecurity.com/index.html")
+    homePage = new HomePage(page) 
+    await homePage.visit()
     })
 
 test("Should find some search results", async ({ page }) =>{
-    
-    await page.type("#searchTerm", "bank")
-    await page.keyboard.press("Enter")
-
+    await homePage.searchForPhrase("bank")
     const numberOfLinks = page.locator('li > a')
     await expect(numberOfLinks).toHaveCount(2)
 
 })
 
 test("No results were found", async ({ page }) => {
-    
-    await page.type("#searchTerm", "adadaadadaadadadd")
-    await page.keyboard.press("Enter")
-    const pageText = await page.locator(".top_offset") 
+    await homePage.searchForPhrase("adadaadadaadadadd")
+    const pageText = page.locator(".top_offset") 
     await expect(pageText).toContainText("No results were found for the query")
 })
 
 test("Validate links content", async ({ page }) => {
-    await page.type("#searchTerm", "money")
-    await page.keyboard.press("Enter")
-    const linkText = await page.locator("ul > li > a")
+    await homePage.searchForPhrase("money")
+    const linkText = page.locator("ul > li > a")
     await expect(linkText).toContainText("Zero - My Money Map")
 })
 
